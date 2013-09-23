82d72c7f32254d28ec1c0ac9dcda0557153d1cb8
Gordon Freeman <svdragster@gmail.com>
2013-09-23 19:33:55 +0200

    Added checking for updates

--- src/de.svdragster.elevator/ElevatorListener.java
+++ src/de.svdragster.elevator/ElevatorListener.java
@@ -1,5 +1,15 @@
 package de.svdragster.elevator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Properties;
+
 import net.canarymod.Canary;
 import net.canarymod.api.entity.living.humanoid.Player;
 /*import net.canarymod.api.factory.ItemFactory;
@@ -13,7 +23,9 @@ import net.canarymod.chat.Colors;
 import net.canarymod.hook.HookHandler;
 import net.canarymod.hook.player.BlockLeftClickHook;
 import net.canarymod.hook.player.BlockRightClickHook;
+import net.canarymod.hook.player.ConnectionHook;
 import net.canarymod.hook.player.SignChangeHook;
+import net.canarymod.plugin.Plugin;
 import net.canarymod.plugin.PluginListener;
 
 public class ElevatorListener implements PluginListener {
@@ -21,15 +33,55 @@ public class ElevatorListener implements PluginListener {
 	public static final String PERMISSION_PLACE = "elevator.place";
 	public static final String PERMISSION_USE = "elevator.use";
 	public static final String PERMISSION_DESTROY = "elevator.destroy";
+	public static final String PERMISSION_UPDATE = "elevator.checkupdates";
 	public static final String SIGN_ELEVATOR = "[Elevator]";
 	public static final int TP_EYE = 1; //Teleport the player, so the Sign is at the height of the eyes of the player.
 	public static final int TP_BELOW = 2; //Teleport the player, so the Sign is at the height of the players legs or feet.
 	public static final int TP_CANCEL = 0; //Cancel the teleport.
+	private static final String USER_AGENT = "Minecraft Server " + Canary.getServer().getName();
+	public static final String VERSION = "1.3";
+	private static final String DIR = "config/elevator/";
+	private static final String FILE = DIR + "elevator.properties";
+	public static boolean CheckForUpdates = false;
 	public int WorldHeight = Canary.getServer().getDefaultWorld().getHeight();
 	boolean AlreadyTeleported = false;
 	
 	public void cast(String string) {
 		Canary.getServer().broadcastMessage(string);
+	}
+	
+	public void createFiles() {
+		File file = new File(FILE);
+		File dir = new File(DIR);
+		Properties props = new Properties();
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
+		if (!file.exists()) {
+			try {
+		        FileOutputStream out = new FileOutputStream(FILE);
+		        props.setProperty("CheckForUpdates", "true");
+		        props.store(out, null);
+		        out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void getProps() {
+		Properties prop = new Properties();
+    	try {
+               //load a properties file
+    		prop.load(new FileInputStream(FILE));
+               //get the property value
+    		String property = prop.getProperty("CheckForUpdates");
+    		if (property.equalsIgnoreCase("true")) {
+    			CheckForUpdates = true;
+    		}
+    	} catch (IOException ex) {
+    		ex.printStackTrace();
+        }
 	}
 	
 	public void CheckForElevator(Sign sign, String direction, Player player) {
@@ -142,6 +194,7 @@ public class ElevatorListener implements PluginListener {
 		if (hook.getBlock().getType().equals(BlockType.WallSign)) {
 			if (hook.getPlayer().isSneaking()) {
 				if (hook.getPlayer().hasPermission(PERMISSION_DESTROY)) {
+					hook.getPlayer().message(Colors.GREEN + "Sign is destroyed.");
 					return;
 				} else {
 					hook.getPlayer().message(Colors.LIGHT_RED + "You don't have the permission to destroy an elevator sign.");
@@ -182,5 +235,59 @@ public class ElevatorListener implements PluginListener {
 				hook.getPlayer().message(Colors.LIGHT_RED + "The sign needs to be a Wall Sign!");
 			}
 		}
+	}
+	
+	@HookHandler
+	public void onLogin(ConnectionHook hook) {
+		if (CheckForUpdates) {
+			if (hook.getPlayer().hasPermission(PERMISSION_UPDATE)) {
+				try {
+					hook.getPlayer().message(Colors.ORANGE + "<Elevator> " + Colors.LIGHT_GRAY + "Checking for updates...");
+					hook.getPlayer().message(sendGet());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public String sendGet() throws Exception {
+		String MYIDSTART = "svdragster>";
+		String MYIDEND = "<svdragster";
+		String url = "http://svcounter.comze.com/checkupdate.php?version=" + VERSION + "&plugin=elevator";
+ 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		// optional default is GET
+		con.setRequestMethod("GET");
+ 
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		//int responseCode = con.getResponseCode();
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		String result = response.toString();
+		if (result.contains(MYIDSTART) && result.contains(MYIDEND)) {
+			//result.replace(MYIDSTART, "");
+			int endPos = result.indexOf(MYIDEND);
+			result = Colors.ORANGE + "<Elevator> " + Colors.GREEN + "Update available at: " + Colors.WHITE + result.substring(MYIDSTART.length(), endPos);
+		} else {
+			result = Colors.ORANGE + "<Elevator> " + Colors.LIGHT_RED + "No update available";
+		}
+		
+		//return result
+		return result;
+ 
 	}
 }
