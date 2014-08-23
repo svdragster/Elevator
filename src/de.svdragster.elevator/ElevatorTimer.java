640fb4a1750484b6b785d046404cd9638b2939b0
Gordon Freeman <svdragster@gmail.com>
2014-08-24 00:17:03 +0200

    Update ElevatorTimer.java

--- src/de.svdragster.elevator/ElevatorTimer.java
+++ src/de.svdragster.elevator/ElevatorTimer.java
@@ -8,6 +8,7 @@ import net.canarymod.Canary;
 import net.canarymod.api.entity.living.humanoid.Player;
 import net.canarymod.api.factory.PacketFactory;
 import net.canarymod.api.packet.Packet;
+import net.canarymod.api.potion.PotionEffectType;
 import net.canarymod.api.world.blocks.Block;
 import net.canarymod.api.world.blocks.BlockType;
 
@@ -28,20 +29,22 @@ public class ElevatorTimer extends TimerTask {
 	
 	@Override
 	public void run() {
-		int d = 1;
+		float d = 0.4f;
 		if (direction <= 0) {
-			d = -1;
+			d = -0.4f;
 		}
 		//player.moveEntity(0, velocity, 0);
-		player.teleportTo(player.getX(), player.getY()+d, player.getZ(), player.getPitch(), player.getRotation());
+		//player.teleportTo(player.getX(), player.getY()+d, player.getZ(), player.getPitch(), player.getRotation());
+		player.setMotionY(d); // I think 0.25 is about a normal jump 
 		Block block = player.getWorld().getBlockAt((int) player.getX(), (int)  player.getY()+1, (int)  player.getZ());
 		if (!exceptions.contains(block.getType()) || player.getY() >= ElevatorListener.WorldHeight || player.getY() < 5) {
 			PacketFactory factory = Canary.factory().getPacketFactory();
 			Packet packet = factory.blockChange((int) player.getX(), (int) player.getY()-1, (int) player.getZ(), 20, 0);
 			player.sendPacket(packet);
 			this.cancel();
-			player.teleportTo(player.getX(), player.getY()+1, player.getZ(), player.getPitch(), player.getRotation());
+			player.teleportTo(player.getX(), player.getY(), player.getZ(), player.getPitch(), player.getRotation());
 		}
+		player.addPotionEffect(PotionEffectType.RESISTANCE, 40, 5);
 	}
 
 	public void setPlayer(Player _player) {
